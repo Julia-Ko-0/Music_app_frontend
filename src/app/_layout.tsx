f7aebc3af92c69663815222e8959326d3946b3e9
Julia-Ko-0 <117535623+Julia-Ko-0@users.noreply.github.com>
2026-08-10 00:44:43 +0300

    fix: configure root Expo Router layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function Index() {
+export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Приветdsd!</Text>
-      {/* <Home></Home> */}
-    </View>
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  text: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-});
