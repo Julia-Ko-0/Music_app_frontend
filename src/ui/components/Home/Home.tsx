b6bde778f82ccc8bfdc7b2cbef67043581c7f6ca
Julia-Ko-0 <117535623+Julia-Ko-0@users.noreply.github.com>
2026-08-10 00:07:38 +0300

    fix: use React Native components in Home screen

--- src/ui/components/Home/Home.tsx
+++ src/ui/components/Home/Home.tsx
@@ -1,10 +1,29 @@
 import { observer } from "mobx-react-lite";
 import { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export const Home: FC = observer(() => {
   return (
-    <div>
-      <h1> hi</h1>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.title}>Главная</Text>
+      <Text style={styles.subtitle}>Главный экран</Text>
+    </View>
   );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 80,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+  },
 });
